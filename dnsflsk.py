5771c62bdd3dd8eee80606f02d46336b2249e873
James Stevens <james@jrcs.net>
2020-01-13 14:05:14 +0000

    Not working, but closer

--- dnsflsk.py
+++ dnsflsk.py
@@ -4,19 +4,37 @@ import flask
 import dns
 
 
+def is_valid_host(host):
+    host = host.encode('idna').lower()
+    if not hasattr(is_valid_host, '_re'):
+        import re
+        is_valid_host._re = re.compile(
+            r'^([0-9a-z][-\w]*[0-9a-z]\.)+[a-z0-9\-]{2,15}$')
+    return bool(is_valid_host._re.match(host))
+
+
+def abort(err_no, message):
+    response = flask.jsonify({'error': message})
+    response.status_code = err_no
+    return response
+
+
 app = flask.Flask("DNS/Rest/api")
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return flask.make_response(flask.jsonify({'error': 'Not found'}), 404)
 
 
 @app.route('/dns/api/v1.0/resolv/', methods=['GET'])
 @app.route('/dns/api/v1.0/resolv', methods=['GET'])
 def resolver():
-	print(flask.request.args.get("name"))
-	return flask.jsonify({ "answer": "This is my answer"})
+    dns_name = flask.request.args.get("name")
+    dns_type = flask.request.args.get("type", default=1, type=int)
+
+    if dns_name is None:
+        return abort(400, "'name' parameter is missing")
+
+    if not is_valid_host(dns_name):
+        return abort(400, "'name' parameter is not a valid FQDN")
+
+    return flask.jsonify({"name": dns_name, "type": dns_type})
 
 
 @app.route("/dns/api/v1.0/")
