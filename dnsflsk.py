ec2f875303e047432965a2535ebca7ed1e6b19b6
James Stevens <james@jrcs.net>
2020-01-13 14:13:40 +0000

    Not working, but closer

--- dnsflsk.py
+++ dnsflsk.py
@@ -4,17 +4,17 @@ import flask
 import dns
 import re
 
-
 is_valid_host_re = re.compile(r'^([0-9a-z][-\w]*[0-9a-z]\.)+[a-z0-9\-]{2,15}$')
 
+
 def is_valid_host(host):
-	return is_valid_host_re.match(host) != None
+    return is_valid_host_re.match(host) is not None
 
 
 def abort(err_no, message):
-	response = flask.jsonify({'error': message})
-	response.status_code = err_no
-	return response
+    response = flask.jsonify({'error': message})
+    response.status_code = err_no
+    return response
 
 
 app = flask.Flask("DNS/Rest/api")
