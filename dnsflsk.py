88b81b697d46b65458afa26733cd9a1976f09e57
James Stevens <james@jrcs.net>
2020-01-13 14:12:16 +0000

    Not working, but closer

--- dnsflsk.py
+++ dnsflsk.py
@@ -2,21 +2,19 @@
 
 import flask
 import dns
+import re
 
+
+is_valid_host_re = re.compile(r'^([0-9a-z][-\w]*[0-9a-z]\.)+[a-z0-9\-]{2,15}$')
 
 def is_valid_host(host):
-    host = host.encode('idna').lower()
-    if not hasattr(is_valid_host, '_re'):
-        import re
-        is_valid_host._re = re.compile(
-            r'^([0-9a-z][-\w]*[0-9a-z]\.)+[a-z0-9\-]{2,15}$')
-    return bool(is_valid_host._re.match(host))
+	return is_valid_host_re.match(host) != None
 
 
 def abort(err_no, message):
-    response = flask.jsonify({'error': message})
-    response.status_code = err_no
-    return response
+	response = flask.jsonify({'error': message})
+	response.status_code = err_no
+	return response
 
 
 app = flask.Flask("DNS/Rest/api")
