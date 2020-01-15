fe7bb5909f72b80ff9f24d614f63ee63c1a35fe0
James Stevens <james@jrcs.net>
2020-01-15 15:53:45 +0000

    Rename tstresolv.py to cmdresolv.py (cmdline) and add cmdline args, add more checking and fix no-repsonse timeout

---
+++ cmdresolv.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+import resolv
+import sys
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument("-s",
+                    '--servers',
+                    help='Comma separated list of server ip addresses')
+
+parser.add_argument("-t",
+                    '--rdtype',
+                    help='Record type to query (number or name), default=A')
+
+parser.add_argument("-n", '--name', help='Name to look-up')
+
+parser.add_argument("-d",
+                    '--do',
+                    help='Set DO bit',
+                    action="store_true",
+                    default=False)
+
+parser.add_argument("-c",
+                    '--cd',
+                    help='Same as --do',
+                    action="store_true",
+                    default=False)
+
+args = parser.parse_args()
+
+args.servers = args.servers.split(",")
+
+c = resolv.Resolver(args)
+ret = c.recv()
+if ret is None:
+    print("Lookup failed")
+else:
+    print(json.dumps(c.recv(), indent=3))
--- resolv.py
+++ resolv.py
@@ -3,26 +3,46 @@
 import socket
 import select
 import os
+import re
 import dns
 import dns.name
 import dns.message
+import dns.rdatatype
 
 DNS_HDR_LEN = 12
 DNS_MAX_RESP = 4096
 MAX_CLIENTS = 10
-MAX_EXPIRY = 30
+MAX_TRIES = 5
+
+is_valid_host_re = re.compile(r'^([0-9a-z][-\w]*[0-9a-z]\.)+[a-z0-9\-]{2,15}$')
+
+
+def is_valid_host(host):
+    return is_valid_host_re.match(host) is not None
 
 
 class Resolver:
     def __init__(self, qry):
+        if not is_valid_host(qry.name):
+            return None
+
+        rdtype = qry.rdtype
+        if rdtype.isdigit():
+            rdtype = int(rdtype)
+        else:
+            rdtype = dns.rdatatype.from_text(rdtype)
+
         self.servers = qry.servers
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.expiry = 1
-        self.question = bytearray(
-            dns.message.make_query(qry.name, qry.rdtype,
-                                   want_dnssec=qry.do).to_wire())
-        if self.question is None:
+        self.expiry = 2
+        self.tries = 0
+        msg = dns.message.make_query(qry.name,
+                                     rdtype,
+                                     want_dnssec=(qry.do or qry.cd))
+        if msg is None:
             return None
+
+        self.question = bytearray(msg.to_wire())
 
     def send_all(self):
         ret = False
@@ -49,20 +69,21 @@ class Resolver:
 
     def recv(self):
         while True:
+            self.tries = self.tries + 1
+
             if not self.send():
                 return None
 
             rlist, wlist, xlist = select.select([self.sock], [], [],
                                                 self.expiry)
             if len(rlist) > 0:
-                break
-            self.expiry = self.expiry + self.expiry
-            if self.expiry > MAX_EXPIRY:
-                return None
+                self.reply, (addr, port) = self.sock.recvfrom(DNS_MAX_RESP)
+                if self.match_id():
+                    return self.decode_reply()
 
-        self.reply, (addr, port) = self.sock.recvfrom(DNS_MAX_RESP)
-        if self.match_id():
-            return self.decode_reply()
+            self.expiry = self.expiry + int(self.expiry / 2)
+            if self.tries >= MAX_TRIES:
+                return None
 
         return None
 
