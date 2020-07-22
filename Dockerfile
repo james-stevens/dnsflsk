21168d25d03c164573483a70aac4357a2327ad00
James Stevens <james@jrcs.net>
2020-07-22 13:03:24 +0000

    fix nginx in read-only, add syslog

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,18 @@ RUN pip install gunicorn
 RUN pip install Flask
 RUN pip install dnspython
 
+RUN rmdir /var/lib/nginx/tmp /var/log/nginx
+RUN ln -s /dev/shm /var/lib/nginx/tmp
+RUN ln -s /dev/shm /var/log/nginx
+RUN ln -s /dev/shm /run/nginx
+
 COPY certkey.pem /etc/nginx/
 RUN rm -f /etc/inittab
 RUN ln -s /ram/inittab /etc/inittab
 RUN ln -s /ram/nginx_ssl.conf /etc/nginx/nginx_ssl.conf
 
 COPY *.py /opt/
-COPY start_wsgi /opt/
-COPY start_nginx /opt/
 RUN python3 -m compileall /opt/
+COPY start start_wsgi start_nginx /opt/
 
-CMD [ /opt/start ]
+CMD [ "/opt/start" ]
--- dnsflsk.py
+++ dnsflsk.py
@@ -5,6 +5,7 @@
 import resolv
 import validation
 import os
+import syslog
 
 import flask
 import dns
@@ -28,12 +29,16 @@ dohServers = "8.8.8.8,8.8.4.4"
 if "DOH_SERVERS" in os.environ:
     dohServers = os.environ["DOH_SERVERS"]
 
+syslogFacility = syslog.LOG_LOCAL6
+syslog.openlog(logoption=syslog.LOG_PID, facility=syslogFacility)
+
 
 @application.route('/dns/api/v1.0/resolv', methods=['GET'])
 def resolver():
     qry = Empty()
     qry.name = flask.request.args.get("name")
     qry.rdtype = flask.request.args.get("type")
+
     qry.servers = flask.request.args.get("servers", default=dohServers)
     qry.ct = flask.request.args.get("ct", default=False, type=bool)
     qry.cd = flask.request.args.get("cd")
@@ -65,6 +70,7 @@ def resolver():
             return abort(400, "'type' parameter is not a known RR name")
 
     try:
+        syslog.syslog("{}/{} -> {}".format(qry.name, dns.rdatatype.to_text(qry.rdtype), qry.servers))
         res = resolv.Resolver(qry)
     except Exception as e:
         return abort(400, e)
